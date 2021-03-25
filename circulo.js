ad6bb77a2ed5f50f35d7c74a2516f6e4ae39a8aa
Fernando-Herrera2018 <fernando-herrera-garnica@hotmail.com>
2021-03-25 07:40:36 -0600

    first commit

---
+++ circulo.js
@@ -0,0 +1,7 @@
+const _PI = Math.PI;
+exports.area = function(r){
+  return _PI * r * r;
+};
+exports.circunferencia = function(r) { 
+  return 2 * _PI * r;
+}; 
---
+++ main.js
@@ -0,0 +1,5 @@
+const circulo = require('./circulo.js'); 
+console.log('Area(radius 4)'+ '=' + circulo.area(4));
+console.log('circunferencia(radius 4)' + '=' + circulo.circunferencia(4));
+console.log('directorio:' + __dirname)
+console.log('nombre de archivo' + __filename)
